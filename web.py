0d4f9683ed1aac197fbf3afb43499133c4d1a1bb
Aniket <aniket.modi117@protonmail.com>
2024-03-24 18:58:48 +0530

    Added warning for same items and about page expander

--- web.py
+++ web.py
@@ -1,28 +1,38 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos()
-
-
 def add_todo():
+    for item in todos:
+        if st.session_state["new_todo"] + "\n" == item:
+            st.warning("You have already entered this to-do item.")
+
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
 
-st.title("My Todo App")
-st.subheader("Add items to do and remember stuff.")
+st.title("A Todo App")
+with st.expander("About this app"):
+    st.info("Add items to-do or simply note them down for future")
 
-cnt = 0
+todos = functions.get_todos()
+
+count = 0
 
 for index, todo in enumerate(todos):
-    cnt += 1
-    checkbox = st.checkbox(todo[:-1], key=f"{todo[:-1]}_{cnt}")
+    count += 1
+    checkbox = st.checkbox(todo[:-1], key=f"{todo[:-1]}-{count}")
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[f"{todo[:-1]}_{cnt}"]
+        del st.session_state[f"{todo[:-1]}-{count}"]
         st.experimental_rerun()
 
-st.text_input(label="Todo Item", placeholder="Add new todo...",
+"stats-for-nerds"
+
+for the_todo in st.session_state.items():
+    the_todo
+
+
+st.text_input(label="Todo Item", placeholder="Add new todo",
               on_change=add_todo, key='new_todo')
